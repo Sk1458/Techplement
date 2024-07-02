2bd3f3699851ed16c643a7712dec098f93213a49
Saikumar Voddepally <dredwing147@gmail.com>
2024-07-02 13:05:24 +0530

    added the generate random quote functionality made the generate function in the clienSide code and /api/quote route for response and handling in the serverSide code respectively
    added a search bar and search button to search quote by author in the client code
    
    added the "search quote by author" functionality
    made the searchQuote function in the clientSide code and /api/search route for response and handling in the serverSide code respectively
    
    added the generate random quote upon refreshing/new opening of the page for better experience.
    
    added a background video for better UI look
    made the continuous playing functionality for the background video even
    when refreshed or new opening of the webpage
    
    modified the html and css files for better looking and responsive userInterface

--- week1-tasks/app.js
+++ week1-tasks/app.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/api/qoutes',  async (req, res) => {
+app.get('/api/quotes',  async (req, res) => {
     const url = 'https://api.api-ninjas.com/v1/quotes';
     const apiKey = process.env.ApiNinjas_key;
 
@@ -25,8 +29,27 @@ app.get('/api/qoutes',  async (req, res) => {
         res.json(response.data);
     }
     catch(error) {
-        res.status(500).json({ error: 'Failed to fetch qoute'});
+        res.status(500).json({ error: 'Failed to fetch quote'});
     }
+});
+
+app.get('/api/search', (req, res) => {
+    const authorName = req.query.author;
+
+    fs.readFile('quotes.json', 'utf8', (err, data) => {
+        if(err) {
+            res.status(500).json({error:`Failed to load the quotes`});
+            return;
+        }
+        const quotes = JSON.parse(data);
+        const quote = quotes.find(q => q.author.toLowerCase() === authorName.toLowerCase());
+
+        if(quote) {
+            res.json(quote);
+        } else {
+            res.status(404).json({error : `Quote not found for the requested author`});
+        }
+    })
 });
 
 app.get('/home', (req, res) => {
